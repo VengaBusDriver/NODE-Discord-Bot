b4c37dd9dc1bbdab614765aad639f213657615e1
Dustin Giovanoli <dustin@emptechllc.na>
2018-06-15 10:43:35 -0400

    added to project - 8ball function

--- app.js
+++ app.js
@@ -5,10 +5,44 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+
+client.on('guildMemberAdd', member => {
+    // Send the message to a designated channel on a server:
+    const channel = member.guild.channels.find('name', 'member-log');
+    // Do nothing if the channel wasn't found on this server
+    if (!channel) return;
+    // Send the message, mentioning the member
+    channel.send(`Welcome to the server, ${member}`);
+  });
+  
+  
 client.on('message', msg => {
-  if (msg.content === 'ping') {
+  if (msg.content === '!Ping') {
     msg.reply('Pong!');
   }
+
+  if (msg.content.includes('!8ball')) {
+    var randomnumber=Math.floor((Math.random()*5) + 1);
+    console.log(randomnumber)
+    switch(randomnumber){
+        case 5:
+        msg.reply('YES!');
+        break;
+        case 1:
+        msg.reply('No!');
+        break;
+        case 2:
+        msg.reply('Outlook not so good!');
+        break;
+        case 3:
+        msg.reply('Ask again later!');
+        break;
+        case 4:
+        msg.reply('You may rely on it!');
+        break;
+      
+    }
+}
 });
 
-client.login('NDU3MTY0NTUzNDI0MzM4OTQ0.DgVIpw.1Au5Q9DCFWFvZYglSDsDWdPetUk');
+client.login('');
